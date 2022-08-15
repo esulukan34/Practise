089859a4745a93a1711b442002e3cc15426ba0d2
Emre Sulukan <esulukan34@gmail.com>
2022-08-15 14:27:08 +0300

    22.08.15

---
+++ src/day3_practice/Q08_MethodCreation.java
@@ -0,0 +1,38 @@
+package day3_practice;
+
+import java.util.Scanner;
+
+public class Q08_MethodCreation {
+     /*
+      Problem Tanımı :
+      Basit 4 islem yapan bir hesap makinesi kodlayiniz....
+      Kullanicidan yapacagi islemi islem sembolu ile secmesini saglayalinız.
+      Kullanicidan iki sayi girmesini isteyiniz.
+      Girilen iki sayi ve secilen isleme gore dogru sonucu ekrana yazdirınız.
+    */
+     public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+
+          System.out.print("birinci sayiyi giriniz : ");
+          int sayi1 = scan.nextInt();
+          System.out.print("ikinci sayiyi giriniz : ");
+          int sayi2 = scan.nextInt();
+
+          System.out.print("islem yapmak istediginiz sembolu seciniz : ");
+          char islem = scan.next().charAt(0);
+
+
+          if (islem == '+') {
+               System.out.println("Girilen sayilarin toplami : " + (sayi1 + sayi2));
+          } else if (islem == '-') {
+               System.out.println("Girilen sayilarin farki : " + (sayi1 - sayi2));
+          } else if (islem == '*') {
+               System.out.println("Girilen sayilarin carpimi : " + (sayi1 * sayi2));
+          } else if (islem == '/') {
+               System.out.println("Girilen sayilarin bolumu : " + (sayi1 / sayi2));
+
+
+          }
+     }
+}
+
---
+++ src/day5_practice/Q01_ForLoop.java
@@ -0,0 +1,63 @@
+package day5_practice;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Q01_ForLoop {
+
+     /*Kullanıcıdan bir harf girmesini isteyiniz. Girilen harf sesli ise Sesli harf olduğunu,
+     değilse sessiz harf olduğunu ekrana yazdırsın. Girdiği değer harf değilse yada
+     1 karakterden fazla ise hata mesajı göstersin. (Test girilen harfi büyük yada küçüklüğüne duyarlıdır.)
+     Sesli harfler: a,e,i,o,u
+
+     Test Data:
+     a
+     Beklenen Çıktı:
+     a harfi sesli harfdir.
+
+     Test Data:
+     d
+     Beklenen Çıktı:
+     d harfi sesiz harftir.
+
+     Test Data:
+     we  yada  %
+
+     Beklenen Çıktı:
+     Yanlis karakter girdiniz!
+
+     */
+     public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+          System.out.print("Lutfen bir harf giriniz : ");
+          String harf = scan.next().toLowerCase();
+
+          String sesliHarfler = "aeiou";
+          String sessizHarfler = "bcdfghjklmnpqrstwxvyz";
+          if (harf.length() == 1) {
+               for (int i = 0; i < sesliHarfler.length(); i++) {
+                    if (sesliHarfler.contains(harf)) {
+                         System.out.println("sesli harf");
+                         break;
+                    } else if (sessizHarfler.contains(harf)) {
+                         System.out.println("sessiz harftir");
+                         break;
+                    } else {
+                         System.out.println("Yanlis karakter girdiniz");
+                         break;
+                    }
+               }
+          } else {
+               System.out.println("Tek karakter giriniz");
+
+
+
+          }
+     }
+}
+
+
+
+
+
