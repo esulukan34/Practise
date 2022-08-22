03b1d7666dca141699cde301acf4901875cb2a2d
Emre Sulukan <esulukan34@gmail.com>
2022-08-22 21:17:12 +0300

    22.08.22

---
+++ src/lambda_practise/Methodlarim.java
@@ -0,0 +1,16 @@
+package lambda_practise;
+
+public class Methodlarim {// seed(tohum) methodlar create edildi
+
+    public static boolean ciftElemaniBul(int x) {
+        return x % 2 == 0;
+        //return x % 2 != 1;
+
+
+
+
+
+    }
+
+
+}
---
+++ src/lambda_practise/Runner01.java
@@ -0,0 +1,28 @@
+package lambda_practise;
+
+public class Runner01 extends Lambda01 {
+
+    public static void main(String[] args) {
+
+        aHarfleriSilenGerisiniYazdiran(names);
+        System.out.println("*******************");
+        maxElemanBul(numbers);
+        System.out.println("*******************");
+        elemanlariTopla(numbers);
+        System.out.println("*******************");
+        elemanlariCarp(numbers);
+        System.out.println("*******************");
+
+        System.out.println("*******************");
+
+        System.out.println("*******************");
+
+        System.out.println("*******************");
+
+        System.out.println("*******************");
+
+
+
+
+    }
+}
